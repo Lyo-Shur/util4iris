cbec9ad1fc142d98d4f73fedc8fbfc1b31ab538e
lyoshur <lyoshur@foxmail.com>
2020-09-03 09:40:42 +0800

    整理代码

--- facade.go
+++ facade.go
@@ -2,31 +2,9 @@ package util4iris
 
 import (
 	"github.com/kataras/iris"
-	"github.com/lyoshur/util4iris/api"
 	"github.com/lyoshur/util4iris/controller"
 	"github.com/lyoshur/util4iris/form"
 )
-
-// api 返回状态码以及数据
-//noinspection GoUnusedConst
-const Success = api.Success
-
-//noinspection GoUnusedConst
-const Fail = api.Fail
-
-//noinspection GoUnusedConst
-const DataAnalysisFailMessage = api.DataAnalysisFailMessage
-
-//noinspection GoUnusedConst
-const ExecuteFailMessage = api.ExecuteFailMessage
-
-//noinspection GoUnusedConst
-const SuccessMessage = api.SuccessMessage
-
-//noinspection GoUnusedConst
-const NullData = api.NullData
-
-type SimpleApi = api.SimpleApi
 
 // controller
 type Factory = controller.Factory
